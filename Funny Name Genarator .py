915bd9c2f48a23278a283d5b8297ce06514f1da7
Tejas K <103427612+FireStromy@users.noreply.github.com>
2023-08-20 19:47:52 +0530

    updated file

--- Funny Name Genarator .py
+++ Funny Name Genarator .py
@@ -1,12 +1,15 @@
 # Funny Name Generator #
 
-# Concepts: print funtion, input function
-
 # Problem
 '''  Take Names from user and generate a funny name '''
 # Sol:
-print("Welcome to Funny Name Generator!!!")
-Fav_City = input("What's the name of the city you hate?\n")
-Fav_Colour = input("What's your favourite colour?\n")
+
+print("*** Welcome to Funny Name Generator!!! ***\n")
+Fav_City = input("What's the name of the city you Like?\n City Name: ")
+print()
+Fav_Colour = input("What's your favourite colour?\n Favourite Colour: ")
 
 print(f"New Funny Name is: {Fav_Colour}er {Fav_City}ly ")
+
+print()
+print("*** Thank you ***")
